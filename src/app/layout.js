b8afa7e0b0f4e1c41705873a179d257975f482b4
Aboud Mohd Bazraa <130313770+AboudBazraa@users.noreply.github.com>
2025-01-05 00:02:02 +0300

    Initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Real Estate Application",
+  description: "A modern web platform for listing and exploring properties such as land plots, rentals, villas, and apartments.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
