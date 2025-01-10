fa7f7511f4b721bb50837c2f6cf7a217358283dc
Aboud Mohd Bazraa <130313770+AboudBazraa@users.noreply.github.com>
2025-01-10 13:47:58 +0300

    Created a Sidebar
    I Well make a side bar from a ShadcnUi component that well use a ( npx shadcn@latest add sidebar-08 )
    to created

--- src/shared/components/NavBar.jsx
+++ src/shared/components/NavBar.jsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <div className="bg-gray-300/75 backdrop-blur-sm absolute">
+    <div className="bg-gray-300/75 backdrop-blur-sm 
+    absolute">
       <nav className="bg-gray-300/75 backdrop-blur-sm sticky top-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex items-center justify-between h-16">
